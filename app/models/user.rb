1a121a78ce7306a6037ec6f00ef4dfa1520a82e3
KY0825 <mouko.ky@gmail.com>
2020-10-15 21:41:35 +0900

    全角カナのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
   validates :nickname,         presence: true
   validates :password,         presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  validates_format_of :password, with: PASSWORD_REGEX 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
   validates :first_name
   validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
   validates :first_name_kana
   validates :last_name_kana
   end
