2e9e6fac4193bc902bf1e63a8e553371d4b67f22
KY0825 <mouko.ky@gmail.com>
2020-10-16 21:02:50 +0900

    ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,9 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def create
+    # if
+    # end
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  validates :image,               presence: true
+  validates :name,                presence: true
+  validates :text,                presence: true
+  validates :category_id,         presence: true
+  validates :status_id,           presence: true
+  validates :delivery_charge_id,  presence: true
+  validates :delivery_area_id,    presence: true
+  validates :days_id,             presence: true
+  validates :price,               presence: true, format: { with: /\A[0-9]+\z/} do   #minimumとmaximum
+  end
+  validates :user,                presence: true
 end
+
+
