6522dcb5af604d6bdd5a3e0d358f2e282f695bef
KY0825 <mouko.ky@gmail.com>
2020-10-17 16:30:11 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   validates :image,               presence: true
   validates :name,                presence: true
   validates :text,                presence: true
@@ -7,9 +9,22 @@ class Item < ApplicationRecord
   validates :delivery_charge_id,  presence: true
   validates :delivery_area_id,    presence: true
   validates :days_id,             presence: true
-  validates :price,               presence: true, format: { with: /\A[0-9]+\z/} do   #minimumとmaximum
+  validates :price,               presence: true, format: { with: /\A[0-9]+\z/} do #minimum: 300,muximum :9999999
   end
   validates :user,                presence: true
+
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :delivery_area
+  belongs_to_active_hash :days
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :delivery_charge_id, numericality: { other_than: 1 } 
+  validates :delivery_area_id, numericality: { other_than: 1 } 
+  validates :days_id, numericality: { other_than: 1 } 
+
 end
 
 
