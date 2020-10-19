630f7936be732541ca6994f4ee358a3461aa5140
KY0825 <mouko.ky@gmail.com>
2020-10-19 14:27:00 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :create
 
-  def index
-    @items = Item.order("created_at DESC")
-  end
+
+  # def index
+  #   @items = Item.order("created_at DESC")
+  # end
 
   def new
     @item = Item.new
