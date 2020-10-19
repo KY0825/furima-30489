ecfbafef9bfa7f35583e08918e587e377fc973b7
KY0825 <mouko.ky@gmail.com>
2020-10-19 14:51:12 +0900

    モデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,19 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  validates :image,               presence: true
-  validates :name,                presence: true
-  validates :text,                presence: true
-  validates :category_id,         presence: true
-  validates :status_id,           presence: true
-  validates :delivery_charge_id,  presence: true
-  validates :delivery_area_id,    presence: true
-  validates :days_id,             presence: true
-  validates :price,               presence: true,:numericality => { only_interger: true,:greater_than_or_equal_to => 300,:less_than_or_equal_to => 9_999_999 },format: { with: /\A[0-9]+\z/} do
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :delivery_area_id
+    validates :days_id
+    validates :price, numericality:{ only_interger: true,greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/}
+    validates :user
   end
-  validates :user,                presence: true
+
   has_one_attached :image
 
   belongs_to :user
@@ -21,10 +23,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_area
   belongs_to_active_hash :days
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :status_id, numericality: { other_than: 1 } 
-  validates :delivery_charge_id, numericality: { other_than: 1 } 
-  validates :delivery_area_id, numericality: { other_than: 1 } 
-  validates :days_id, numericality: { other_than: 1 } 
-
+  with_options other_than: 1 do
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :delivery_area_id
+    validates :days_id
+  end
 end
