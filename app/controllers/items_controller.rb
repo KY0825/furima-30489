1d035c1ab718ce441591f9e9f9c4ee89ee0c95de
KY0825 <mouko.ky@gmail.com>
2020-10-20 19:19:50 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :user_check, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :user_check, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -30,6 +30,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    end
+  end
 
   private
 
