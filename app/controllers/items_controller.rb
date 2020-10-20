6403d76caf39f1828c5d09830d08ce177f59d015
KY0825 <mouko.ky@gmail.com>
2020-10-20 16:11:03 +0900

    itemコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new]
-
+  before_action :edit_user_item, only: :edit
 
   def index
     @items = Item.order("created_at DESC")
@@ -24,10 +24,34 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
   def item_params
     params.require(:item).permit(:image,:name,:text,:category_id,:status_id,:delivery_charge_id,:delivery_area_id,:days_id,:price).merge(user_id: current_user.id)
+  end
+
+
+  def edit_user_item
+    unless user_signed_in?
+      redirect_to action: :new
+    end
   end
 end
