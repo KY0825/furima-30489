f699b6f1c756a19b64f2555989695a3fc8f82007
KY0825 <mouko.ky@gmail.com>
2020-10-22 15:26:44 +0900

    ordersコントローラーの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,25 @@
 class OrdersController < ApplicationController
 
-  def
-    index
+  def index
+  end
+
+  def new
+    @user_order = UserOrder.new
+  end
+
+  def create
+    @user_order = UserOrder.create(user_order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect to action: :index
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def user_order
+    params.require(:user_order).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user, :item)
   end
 end
