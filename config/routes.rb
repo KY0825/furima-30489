a8035f6158463f48947744cf8c3eaf2a7fb61d80
KY0825 <mouko.ky@gmail.com>
2020-10-22 15:30:36 +0900

    ordersのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :orders, only: [:index, :new, :create]
 end
