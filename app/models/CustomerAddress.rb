90e667ed6ef551ab6764dde008206861aca705ad
KY0825 <mouko.ky@gmail.com>
2020-10-22 16:34:25 +0900

    CustomerAddressの修正

--- app/models/CustomerAddress.rb
+++ app/models/CustomerAddress.rb
@@ -1,13 +1,3 @@
 class CustomerAddress < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code,format: { with: /\A[0-9]+\z/}
-    validates :prefecture_id
-    validates :city
-    validates :address
-    validates :phone_number,format: { with: /\A[0-9]+\z/}
-    validates :user
-  end
-
-  validates :prefecture_id,numericality: {other_than: 1}
-
+  belongs_to :order
 end
