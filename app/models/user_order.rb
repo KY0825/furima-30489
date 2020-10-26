cab8e0dc7587897da6c237f99492329f3baf9c9a
KY0825 <mouko.ky@gmail.com>
2020-10-26 14:04:06 +0900

    user_orderモデルの修正

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,28 +1,23 @@
 class UserOrder
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user, :item, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   # Orderに関するバリデーション
-  validates :price, presence: true
   validates :token, presence: true
   # //Orderに関するバリデーション
   # CustomerAddressに関するバリデーション
   with_options presence: true do
-    validates :postal_code,format: { with: /\A[0-9]+\z/}
-    validates :prefecture_id
+    validates :postal_code,format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefecture_id,numericality: {other_than: 1}
     validates :city
     validates :address
-    validates :phone_number,format: { with: /\A[0-9]+\z/}
-    validates :user
+    validates :phone_number,format: { with: /\A\d{11}\z/}
   end
   # //CustomerAddressに関するバリデーション
-  # CustomerAddressの都道府県に関するバリデーション
-  validates :prefecture_id,numericality: {other_than: 1}
-  # //CustomerAddressの都道府県に関するバリデーション
 
   def save
-    order = Order.create(user: user, item: item)
-    customer_address = CustomerAddress.create(postal_code: postal_code ,prefecture_id: prefecture_id,city: city, address: address, building_name: building_name, phone_number: phone_number)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    customer_address = CustomerAddress.create(postal_code: postal_code ,prefecture_id: prefecture_id,city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
