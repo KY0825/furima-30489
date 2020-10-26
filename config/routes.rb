de99797f86c8bc97f6a93d5550cc38859f7a65d1
KY0825 <mouko.ky@gmail.com>
2020-10-26 15:43:13 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :orders, only: [:index, :create, :new]
+  resources :items do
+    resources :orders, only: [:index, :create]
   end
 end
