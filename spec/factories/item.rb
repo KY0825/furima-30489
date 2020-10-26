cdbda5bb77aa25a20c53ef39b6cd1e0b856bd2fa
KY0825 <mouko.ky@gmail.com>
2020-10-26 15:43:32 +0900

    spec/factories/item.rbの修正

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     name                    {"テスト"}
     text                    {"テスト"}
-    category_id             {"2"}
-    status_id               {"2"}
-    delivery_charge_id      {"2"}
-    delivery_area_id        {"2"}
-    days_id                 {"2"}
+    category_id             { 2 }
+    status_id               { 2 }
+    delivery_charge_id      { 2 }
+    delivery_area_id        { 2 }
+    days_id                 { 2 }
     price                   {"3000"}
 
     association :user
